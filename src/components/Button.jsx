d076454d936915e9778c9098ff759d39c82db4db
asepur <asepur94@gmail.com>
2021-11-20 00:57:50 +0100

    inicializando react

---
+++ src/components/Button.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "../app/css/button.css"
+
+
+const Button = () => {
+    return (
+        <button className="btn">
+            botón
+        </button>
+
+    )
+}
+
+export default Button
